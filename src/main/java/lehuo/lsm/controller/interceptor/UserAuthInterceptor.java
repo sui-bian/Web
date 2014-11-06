f23c28ef640f31893ba47c5c3b8736f297423082
simon <lsm0622@163.com>
2014-11-06 21:43:00 +0800

    wq

--- src/main/java/lehuo/lsm/controller/interceptor/UserAuthInterceptor.java
+++ src/main/java/lehuo/lsm/controller/interceptor/UserAuthInterceptor.java
@@ -7,9 +7,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 
 /**
@@ -25,12 +27,21 @@ public class UserAuthInterceptor extends HandlerInterceptorAdapter {
 
     private static final String INTERCEPTOR_URL = "/admin/";
 
+    private String noAuthView = "/WEB-INF/jsp/noAuth.jsp";
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         logger.info("admin interceptor {},{}",request.getRequestedSessionId(),request.getRequestURI());
+        failAccess(request, response);
         return true;
 
+    }
+
+    private void failAccess(HttpServletRequest request, HttpServletResponse response) throws IOException,
+            ServletException {
+        logger.info("权限未通过验证，跳转至无权限页面:{}", noAuthView);
+        request.getRequestDispatcher(noAuthView).forward(request, response);
     }
 
 }
